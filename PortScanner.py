c8748e63702abbaf5cd141d9ba6c44ad6d237f4a
EsiAmartey <49249453+EsiAmartey@users.noreply.github.com>
2023-06-28 18:25:57 -0500

    Add files via upload

--- PortScanner.py
+++ PortScanner.py
@@ -1,25 +1,30 @@
 import socket
+import threading
+from concurrent.futures import ThreadPoolExecutor
+
+def scan_port(target_host, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(1)
+
+    try:
+        result = sock.connect_ex((target_host, port))
+        if result == 0:
+            try:
+                service = socket.getservbyport(port)
+            except socket.error:
+                service = "Unknown"
+            print(f"Port {port} is open - Service: {service}")
+    except socket.error:
+        pass
+    finally:
+        sock.close()
 
 def scan_ports(target_host, start_port, end_port):
     print(f"Scanning ports on {target_host}...")
     
-    # Iterate over the range of ports
-    for port in range(start_port, end_port + 1):
-        # Create a socket object
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(1)  # Set a timeout for the connection attempt
-        
-        # Try to connect to the port
-        result = sock.connect_ex((target_host, port))
-        
-        # Check if the port is open
-        if result == 0:
-            print(f"Port {port} is open")
-        else:
-            print(f"Port {port} is closed")
-        
-        # Close the socket
-        sock.close()
+    with ThreadPoolExecutor() as executor:
+        for port in range(start_port, end_port + 1):
+            executor.submit(scan_port, target_host, port)
 
 # Example usage
 target_host = "example.com"  # Replace with the target host or IP address
